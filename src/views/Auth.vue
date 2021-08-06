8c0bbb649884ab1f42f9d1d4570eadd51d1ac48f
{s246294@wakayama-u.ac.jp} <s246294@wakayama-u.ac.jp>
2021-08-06 15:54:09 +0900

    名前をDBから取得

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -23,16 +23,17 @@ export default{
     signUp: function () {
       var provider = new firebase.auth.GoogleAuthProvider()
       firebase.auth().signInWithPopup(provider)
-        .then(obj => {
-          //データベースに登録
-          const userData = {
-            mail: result.additionalUserInfo.profile.mail,
-            userName: result.additionalUserInfo.profile.given_userName,
-          }
-          alert('Create account: ' + obj.user.displayName)
+        .then((result) => {
+          alert('Create account: ' + result.user.displayName)
           this.$store.commit('setIsLoggedIn', true);
+          const userData = {
+            id: result.user.uid,
+            userName: result.additionalUserInfo.profile.given_name,
+            mail: result.additionalUserInfo.profile.email,          
+          }
           console.log(userData)
           db.firestore().collection('users').doc(result.user.uid).set(userData)
+          this.$store.commit('setUserInfo', userData.userName);
           this.$router.push('/')
         })
         .catch(error => alert(error.message))
