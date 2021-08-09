752b43317fc407feb353790a7e9fa750b79cbdda
{s246207@wakayama-u.ac.jp} <78949102+3rai@users.noreply.github.com>
2021-08-09 14:31:00 +0900

    mainの画像を正方形にした

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -4,7 +4,7 @@
       <h1>マイページ</h1>
       <div class="user-info">
         <div class="photo1">
-          <img src="@/assets/S__852356.jpg">
+          <img src="@/assets/S__852356.jpg" />
         </div>
         <h2>なまえ</h2>
         <p1>根本</p1>
@@ -16,107 +16,114 @@
     </div>
 
     <div id="main">
-      <div class="pictures" v-for="test,index in testarray" :key="index">
-        <img v-bind:src="test.picture">
+      <div
+        class="pictures"
+        v-for="(test, index) in testarray"
+        :key="index"
+      >
+        <img v-bind:src="test.picture" />
       </div>
     </div>
 
     <ul id="nav">
-      <li><i class="fas fa-home fa-lg"></i><a href="http://localhost:8080/">ホーム</a></li>
-      <li><i class="fas fa-user fa-lg"></i><a href="http://localhost:8080/MyPage">マイページ</a></li>
-      <li><i class="fas fa-cog fa-lg"></i><a href="http://localhost:8080/register">編集</a></li>
+      <li>
+        <i class="fas fa-home fa-lg"></i
+        ><a href="http://localhost:8080/">ホーム</a>
+      </li>
+      <li>
+        <i class="fas fa-user fa-lg"></i
+        ><a href="http://localhost:8080/MyPage">マイページ</a>
+      </li>
+      <li>
+        <i class="fas fa-cog fa-lg"></i
+        ><a href="http://localhost:8080/register">編集</a>
+      </li>
     </ul>
 
     <!-- <div class="home-cta" >
           <i class="fas fa-home fa-lg"></i>
     </div> -->
-
   </div>
-
 </template>
 
 <script>
-export default{
-  name: 'mypage',
-  data () {
+export default {
+  name: "mypage",
+  data() {
     return {
-      testarray:{
-        "one":{
-          "picture": require("@/assets/S__884742.jpg")
-        }, 
-        "two":{
-          "picture": require("@/assets/S__884744.jpg")
-        }, 
-        "three":{
-          "picture": require("@/assets/S__884745.jpg")
-        }, 
-        "four":{
-          "picture": require("@/assets/S__884746.jpg")
+      testarray: {
+        one: {
+          picture: require("@/assets/S__884742.jpg"),
         },
-        "five":{
-          "picture": require("@/assets/S__884747.jpg")
+        two: {
+          picture: require("@/assets/S__884744.jpg"),
         },
-        "six":{
-          "picture": require("@/assets/S__884748.jpg")
+        three: {
+          picture: require("@/assets/S__884745.jpg"),
         },
-        "seven":{
-          "picture": require("@/assets/S__892931.jpg")
+        four: {
+          picture: require("@/assets/S__884746.jpg"),
         },
-        "eight":{
-          "picture": require("@/assets/S__892933.jpg")
+        five: {
+          picture: require("@/assets/S__884747.jpg"),
         },
-        "nine":{
-          "picture": require("@/assets/S__892934.jpg")
+        six: {
+          picture: require("@/assets/S__884748.jpg"),
         },
-        "ten":{
-          "picture": require("@/assets/S__835622.jpg")
+        seven: {
+          picture: require("@/assets/S__892931.jpg"),
         },
-        "eleven":{
-          "picture": require("@/assets/S__835622.jpg")
-        }
-      }
-    }
+        eight: {
+          picture: require("@/assets/S__892933.jpg"),
+        },
+        nine: {
+          picture: require("@/assets/S__892934.jpg"),
+        },
+        ten: {
+          picture: require("@/assets/S__835622.jpg"),
+        },
+        eleven: {
+          picture: require("@/assets/S__835622.jpg"),
+        },
+      },
+    };
   },
-  methods: {
-    
-  }
-}
-
-
-
+  methods: {},
+};
 </script>
 
 <style scoped>
-
-#mypage{
+#mypage {
   width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
 }
 
-h1{
+h1 {
   color: rgb(82, 74, 7);
   background: #f0d069;
-  text-align:center;
+  text-align: center;
   font-size: 30px;
   margin-bottom: 10px;
-  font-family: 'メイリオ', Meiryo,'ヒラギノ角ゴシック','Hiragino Sans','Hiragino Kaku Gothic ProN','ヒラギノ角ゴ ProN W3',sans-serif;
+  font-family: "メイリオ", Meiryo, "ヒラギノ角ゴシック", "Hiragino Sans",
+    "Hiragino Kaku Gothic ProN", "ヒラギノ角ゴ ProN W3", sans-serif;
 }
 
-.user-info{
+.user-info {
   width: 100%;
   display: flex;
   align-items: center;
   flex-direction: column;
 }
 
-h2{
+h2 {
   font-size: 20px;
   text-align: center;
   padding-left: 10px;
   margin-top: 5px;
-  text-decoration:underline;
-  text-decoration-color:#849e26;
+  text-decoration: underline;
+  text-decoration-color: #849e26;
 }
 
 .photo1 {
@@ -125,60 +132,73 @@ h2{
   /* border-radius: 50%; */
 }
 
-.photo1 img{
+.photo1 img {
   width: 100%;
   height: 100%;
   object-fit: cover;
   border-radius: 50%;
 }
 
-#main img{
+#main img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: 0;
   width: 100%;
   height: 100%;
   object-fit: cover;
 }
 
-p1{
+p1 {
   font-size: 30px;
   margin-left: 20%;
   margin-right: 20%;
 }
 
-p2{
+p2 {
   font-size: 20px;
   margin-left: 20%;
   margin-right: 20%;
 }
 
-#main{
+#main {
   width: 90%;
   display: flex;
   gap: 0 3.5%;
   flex-wrap: wrap;
   /* overflow: scroll;  */
   /* height: 100vh;  */
-  margin-left: auto;
-  margin-right: auto;
   background: #dfd9c7;
   margin-top: 30px;
   /* justify-content: space-between; */
+  /* justify-content: space-around; */
 }
 
-.pictures{
+.pictures {
+  position: relative;
   width: 31%;
-  height: auto;
   margin-bottom: 3%;
-} 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.pictures::before{
+  content: "";
+  padding-top: 100%;
+  display: block;
+}
 
 #nav {
   list-style: none;
 }
- 
+
 #nav li {
   width: 140px;
   text-align: center;
 }
- 
+
 #nav {
   list-style: none;
   overflow: hidden;
@@ -186,7 +206,7 @@ p2{
   width: 100%;
   bottom: 0;
 }
- 
+
 #nav li {
   width: 33.3%;
   text-align: center;
@@ -196,7 +216,7 @@ p2{
   line-height: 50px;
   /* margin-right: 2px; */
 }
- 
+
 #nav li a {
   text-decoration: none;
   color: rgb(253, 251, 244);
@@ -208,6 +228,4 @@ p2{
   height: 35px;
   position: sticky;
 }
-
-
 </style>
